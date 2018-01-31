368bd52d88ebf2530bf78f08c946d89371219819
Mars Wong <marswong618@gmail.com>
2018-01-31 04:06:23 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,12 +13,10 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.valid?
-      if @status == "pending"
-        @sender.balance -= @amount
-        @receiver.balance += @amount
-        @status = "complete"
-      end
+    if @sender.valid? && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
     else
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
